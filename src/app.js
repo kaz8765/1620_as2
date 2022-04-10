fb64fd8521f6d5e22747c686ff5a2fdece87fd23
Kazi <kazi_w@hotmail.com>
2022-04-10 13:43:30 -0700

    update JS and CSS to add notes to note array

--- src/app.js
+++ src/app.js
@@ -1,133 +1,88 @@
-const textArea=`<div class="textarea">
-<textarea id="story" name="story" rows="5" cols="33" enabled>
+//List of notes
+const notesList = [
+    { 
+      title: "first note", 
+      noteBody: "this is an example note",
+      id: 1 
+    }
+  ]
 
+//Target area to store note in
+const noteList=document.querySelector('.notes-list')
+noteList.innerHTML='<li>'+notesList[0].title+'</li>'
 
-
+// Define text area
+const textArea=`
+<textarea id="note" name="story" rows="5" cols="33" enabled>
 </textarea>
-
-
 <div class="options">
     <div class="a">save</div>
     <div class="b">cancel</div>
-
 </div>
 </div>`
 
-
-
+//Target + sign 
 const plusSign=document.querySelector('i')
+
+//Target area to write note in
 const noteWriteArea=document.querySelector('.write-note-area')
 
+
+
+
+
+
+//Function to display textArea in correct area and target new DOM objects
 
 
 function displayNote(evt){
   
   noteWriteArea.insertAdjacentHTML('beforeend',textArea)
+
+  //Target new DOM objects the cancel and save button
   const cancelButton = document.querySelector('.b')
+  const saveButton=document.querySelector('.a')
+  
   cancelButton.addEventListener('click', removeNote)
-  
-  
+  saveButton.addEventListener('click', createNote)
+  saveButton.addEventListener('click', removeNote)
 }
-// plusSign.addEventListener('click',displayNote)
+
+//Ensure clicking plus sign does not create duplicate text areas hence once:true
+
+plusSign.addEventListener('click',displayNote,{once:true});
 
 
-
-
-plusSign.addEventListener('click',displayNote,{once:false});
-
+//Remove note function
 
 function removeNote(evt){
   noteWriteArea.innerHTML=''
+//Re-initialize the display area when clicking + after closing by save or cancel
+
+  plusSign.addEventListener('click',displayNote)
+  }
+
   
+//Function to store note 
+
+function createNote(evt){
+//Selects text area
+  const textArea=document.querySelector('textarea#note')
+//Saves entire text
+  const textAreaValue=textArea.value
+//Splits text into an array
+  const noteArray=textAreaValue.split('\n')
+//Assigns first line as title
+  const noteTitle=noteArray[0]
   
+  notesList.push({title:noteTitle,noteBody:textAreaValue, id:notesList.length+1})
+
+  console.log(notesList)
+  noteList.insertAdjacentHTML('afterbegin','<li>'+noteTitle +'</li>')
+  const styling=document.querySelector('.notes-list li')
+  styling.classList.add('.notes-list li')
+ 
+
 }
 
 
-
-
-// const notes = [
-//   { 
-//     title: "first note", 
-//     noteBody: "this is an example note",
-//     id: 1 
-//   }
-// ]
-
-// function displayNote(note){
-//   const noteDisplayArea=document.querySelector('#write-text')
-//   noteDisplayArea.innerHTML=''
-//   noteDisplayArea.insertAdjacentHTML('afterbegin',note)
-// }
-/*function newNote(noteBody){
-  const template=`<p>${noteBody}</p>`
-  return template
-}
-
-
-
-function getNote(){
-  const textArea=document.querySelector('#note-input')
-  const note=textArea.value
-  return note
-}
-
-function assembleNote(){
-  const noteText=getNote()
-  const note=newNote(noteText)
-  displayNote(note)
-}
-
-saveTextBtn.addEventListener('click',assembleNote)*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function opentextarea() {
-//   var input = document.createElement('textarea');
-//   input.name = 'post';
-//   input.maxLength = 5000;
-//   input.cols = 80;
-//   input.rows = 40;
-//   input.className = 'myCustomTextarea';
-//   var button = document.createElement('button');
-//   var oBody = document.getElementById("body");
-//   while (oBody.childNodes.length > 0) {
-//       oBody.removeChild(oBody.childNodes[0]);
-//   }
-//   oBody.appendChild(input);
-//   oBody.appendChild(button);
-// }
-
-// const input = document.createElement("input");
-//     input.setAttribute("type", "text");
-//     document.body.appendChild(input);
-
-
-
-
-
-
-// const textArea=<textarea>ffsdfsdf</textarea>
-// const selectItem = document.querySelector(`.icons`)
-// const makeDuplicate = document.querySelector(`.icons`)
-
-// function copydiv(evt) {
-//     selectItem.addEventListener(`click`, evt => {
-//         makeDuplicate.insertAdjacentHTML(`afterbegin`, evt.target.innerHTML + textArea)
-
-//     })
-// }
-// copydiv()
